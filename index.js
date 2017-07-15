e3ceda162a76dbda0590039f017b8e72f8494e39
Henrik Ruscon <henrik.dahlheim@gmail.com>
2017-07-15 17:57:04 +0200

    fix colors

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // Syntax scheme
 const backgroundColor   = '#0F1C21';
 const foregroundColor   = '#80B6AD';
-const cursorColor       = '#80B6AD';
-const borderColor       = '#12232A';
+const cursorColor       = '#00485A';
+const borderColor       = '#14262C';
 const colors            = {
       black             : backgroundColor,
       red               : '#046655',
       green             : '#04C975',
       yellow            : '#B5BD68',
-      blue              : '#3F5E61',
+      blue              : '#308891',
       magenta           : '#94A5BC',
       cyan              : '#8ABEB7',
       white             : foregroundColor,
-      lightBlack        : '#434A58',
+      lightBlack        : '#3F5E61',
       lightRed          : '#046655',
       lightGreen        : '#04C975',
       lightYellow       : '#B5BD68',
-      lightBlue         : '#81A2BE',
+      lightBlue         : '#308891',
       lightMagenta      : '#94A5BC',
       lightCyan         : '#8ABEB7',
       lightWhite        : foregroundColor
